b78049146b8eafb03c378830db2961aa1518185c
LimHM <gusals3407@gmail.com>
2017-02-13 11:10:27 +0900

    member, post app 생성

--- django_app/instagram/settings.py
+++ django_app/instagram/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'member.apps.MemberConfig',
+    'post.apps.PostConfig',
 ]
 
 MIDDLEWARE = [
