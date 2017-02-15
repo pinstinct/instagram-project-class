2d3991696ca66de65c6d91854149cd2d8e706a53
LimHM <gusals3407@gmail.com>
2017-02-15 20:22:16 +0900

    댓글 달기 기능 추가

--- django_app/post/urls.py
+++ django_app/post/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'post'
 urlpatterns = [
     url(r'^$', views.post_list, name='list'),
-    url(r'^(?P<post_id>[0-9]+)/', views.post_detail, name='detail'),
+    url(r'^(?P<post_id>[0-9]+)/$', views.post_detail, name='detail'),
+    url(r'^(?P<post_id>[0-9]+)/comment/add/$', views.comment_add, name='comment_add'),
 ]
--- django_app/post/views.py
+++ django_app/post/views.py
@@ -14,10 +14,17 @@ Post Detail (하나의 Post에 대한 상세화면
 5. '/post/<숫자>/'로 접속했을 때 post_detail View에 연결되도록 post/urls.py에 내용 작성
 이 때, post_id 라는 패턴명을 가지도록 정규표현식 작성
 6. url인자로 전달받은 post_id에 해당하는 Post객체를 context에 넘겨 post_detail 화면을 구성
+
+Post Detail에 댓글 작성기능 추가
+1. request.method에 따라 로직 분리되도록 if/else 블록 추가
+2. request.method가 POST일 경우, request.POST에서 'content' 키의 값을 가져옴
+3. 현재 로그인한 유저는 request.user로 가져오며 Post의 id값은 post_id 인자로 전달되므로 두 내용을 사용
+4. 위 내용들과 content를 사용해서 Comment 객체 생성 및 저장
+5. 다시 아까 페이지 (Post Detail)로 redirect
 """
 
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, redirect
+from .models import Post, Comment
 
 
 def post_list(request):
@@ -34,3 +41,21 @@ def post_detail(request, post_id):
         'post': post
     }
     return render(request, 'post/post_detail.html', context)
+
+
+def comment_add(request, post_id):
+    if request.method == 'POST':
+        content = request.POST['content']
+        # 세션 미들위어가 request에 user객체를 넣어 보내줌
+        user = request.user
+        post = Post.objects.get(id=post_id)
+
+        # 아래와 동일한데 모델에 구현한 기능을 사용
+        # post.add_comment(user, content)
+
+        Comment.objects.create(
+            author=user,
+            post=post,
+            content=content,
+        )
+        return redirect('post:detail', post_id=post_id)
