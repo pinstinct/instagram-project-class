10ee6ca0650f54ee3c8947f10f82229566b6b4a7
LimHM <gusals3407@gmail.com>
2017-02-16 23:07:25 +0900

    필요없는 코드 삭제

--- django_app/instagram/urls.py
+++ django_app/instagram/urls.py
@@ -30,7 +30,4 @@ urlpatterns += static(
     settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT
 )
-if settings.DEBUG:
-    urlpatterns += [
-        url(r'^static/(?P<path>.*)$', views.serve),
-    ]
+
