715cced8e203e33b4be3f52e4a83ba349f3b43c9
LimHM <gusals3407@gmail.com>
2017-02-20 17:37:31 +0900

    login_required 데코레이터 사용을 위해 LOGIN_URL 추가

--- django_app/instagram/settings.py
+++ django_app/instagram/settings.py
@@ -95,6 +95,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+LOGIN_URL = 'member:login'
 AUTH_USER_MODEL = 'member.MyUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
